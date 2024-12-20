3556526407a72e63194e2835bbc3f994cc2d5ed3
Sp <sudeeppatil.1810.sp@gmail.com>
2024-12-20 12:10:48 +0530

    Add project files.

---
+++ MovieCore/Data/MovieCoreContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MovieCore.Models;
+
+namespace MovieCore.Data
+{
+    public class MovieCoreContext : DbContext
+    {
+        public MovieCoreContext (DbContextOptions<MovieCoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MovieCore.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ MovieCore/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MovieCore.Data;
+using MovieCore.Models;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<MovieCoreContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MovieCoreContext") ?? throw new InvalidOperationException("Connection string 'MovieCoreContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var services
+        = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
